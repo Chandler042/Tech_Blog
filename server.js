587416d6a54014872da4554686ec5628160c8ac8
chandler042 <Chandler042@gmail.com>
2021-07-12 20:00:14 -0400

    heroku app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const expresshandlebars = require("express-handlebars");
 const routes = require("./controllers");
-
+const { env } = require('process');
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
